9d872cb972ef261241a23716b1bedc9d5a375938
Chett4 <129016266+Chett4@users.noreply.github.com>
2024-01-17 16:05:35 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const myArray = [1, 2, 3, 4, 5];
+
+const printArrayElements = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+};
+
+printArrayElements(myArray);
